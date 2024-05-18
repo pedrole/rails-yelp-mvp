c2af21fe36bf39886db62530d84489b4c9aad51e
Pedro Leal <lealpedro2009@gmail.com>
2024-05-18 15:48:29 +0100

    Added new review form to show restaurant page

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   resources :restaurants, only: %i[index new create show] do
-    resources :reviews, only: %i[new create]
+    resources :reviews, only: %i[create]
   end
 
 end
