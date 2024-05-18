dbd04462e903647a413e8225c74d6d2e299dd7b3
Pedro Leal <lealpedro2009@gmail.com>
2024-05-18 15:39:45 +0100

    Solved Yelp MVP

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,8 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+5.times do
+  Restaurant.create!(name: Faker::Name.name, address: Faker::Address.full_address,
+     phone_number: Faker::PhoneNumber.phone_number, category: Restaurant::CATEGORIES.sample)
+end
